505f8507e31bcb13539b6cde116c5b66c6b57987
Mike Melchior <mike.melchior@outlook.com>
2023-03-14 18:16:25 -0400

    wrong file commit, remove comments add test success comments

--- recursion.js
+++ recursion.js
@@ -1,30 +1,42 @@
-// const fibs = (n) => {
-//     if (n == 1) return [0]
-//     if (n == 2) return [0, 1]
-//     let arr = [0, 1];
-//     let prev = 0;
-//     let curr = 1;
-//     let result = prev + curr;
-//     for (let i = 2; i < n; i++) {
-//         arr.push(result);
-//         prev = curr;
-//         curr = result;
-//         result = prev + curr;
-//     }
-//     return arr
-// }
+const fibs = (n) => {
+    if (n == 1) return [0]
+    if (n == 2) return [0, 1]
+    let arr = [0, 1];
+    let prev = 0;
+    let curr = 1;
+    let result = prev + curr;
+    for (let i = 2; i < n; i++) {
+        arr.push(result);
+        prev = curr;
+        curr = result;
+        result = prev + curr;
+    }
+    return arr
+}
 
 
-// let fibsRec = (n) => {
-//     if (n < 0) {
-//         'Please enter a valid number'
-//     } else if ( n === 1) {
-//         return [0];
-//     } else if (n === 2) {
-//         return [0, 1]
-//     } else {
-//         return [...fibsRec(n-1), fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]]
-//     }
-// }
+// iterative version
+//
+// fibs(4) ...//... [0, 1, 1, 2];
+// fibs(5) ...//... [0, 1, 1, 2, 3];
+// fibs(10) ...//... [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
 
-// console.log(fibsRec(4))  .... [0, 1, 1, 2]
+
+
+let fibsRec = (n) => {
+    if (n < 0) {
+        'Please enter a valid number'
+    } else if ( n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1]
+    } else {
+        return [...fibsRec(n-1), fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]]
+    }
+}
+
+// recursive 
+//
+// fibsRec(4)  ...//... [0, 1, 1, 2];
+// fibsRec(6)  ...//... [0, 1, 1, 2, 3, 5];
+// fibsRec(10) ...//... [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
