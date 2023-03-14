299a997c9903d33582fca2181f5dbc64f950e206
Mike Melchior <mike.melchior@outlook.com>
2023-03-14 18:11:04 -0400

    TOP merge sort project working as intended on commit

---
+++ merge-sort.js
@@ -0,0 +1,42 @@
+const merge = (listA, listB) => {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    let n = listA.length;
+    let m = listB.length;
+    let listC = [];
+    while (i < n && j <m) {
+        if (listA[i] < listB[j]) {
+            listC[k++] = listA[i++]
+        } else {
+            listC[k++] = listB[j++]
+        }
+    }
+    for (; i < n; i++) {
+        listC[k++] = listA[i];
+    }
+    for (; j < m; j++) {
+        listC[k++] = listB[j];
+    }
+    
+    return listC;
+}
+
+// merge([2, 4, 6, 8], [1, 3, 5, 7]) ...//... [1, 2, 3, 4, 5, 6, 7, 8]
+// merge([2], [1]) ...//...[1, 2]
+
+const mergeSort = (list) => {
+    start = 0;
+    finish = list.length;
+    if (finish < 2) {
+        return list;
+    }
+    let mid = Math.floor((start + finish) / 2);
+    let x = mergeSort(list.slice(0, mid))
+    let y = mergeSort(list.slice(mid))
+    return merge(x, y);
+}
+
+
+// mergeSort([3, 1, 2, 4, 7, 5, 6, 8]) ...//... [1, 2, 3, 4, 5, 6, 7, 8]
+// mergeSort([2, 6, 2, 75, 2, 4, 8, 3, 57, 2, 35]) ...//... [2, 2, 2, 2, 3, 4, 6, 8, 35, 57, 75]
