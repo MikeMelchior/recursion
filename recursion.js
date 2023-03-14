321d214db64b230bf0b7c6b14e8361a1448006a5
Mike Melchior <mike.melchior@outlook.com>
2023-03-14 18:10:20 -0400

    TOP project: merge sort algorithm, working as intended on commit

--- recursion.js
+++ recursion.js
@@ -14,18 +14,17 @@
 //     return arr
 // }
 
-// console.log(fibs(5));
 
-let fibsRec = (n) => {
-    if (n < 0) {
-        'Please enter a valid number'
-    } else if ( n === 1) {
-        return [0];
-    } else if (n === 2) {
-        return [0, 1]
-    } else {
-        return [...fibsRec(n-1), fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]]
-    }
-}
+// let fibsRec = (n) => {
+//     if (n < 0) {
+//         'Please enter a valid number'
+//     } else if ( n === 1) {
+//         return [0];
+//     } else if (n === 2) {
+//         return [0, 1]
+//     } else {
+//         return [...fibsRec(n-1), fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]]
+//     }
+// }
 
-console.log(fibsRec(4));
+// console.log(fibsRec(4))  .... [0, 1, 1, 2]
